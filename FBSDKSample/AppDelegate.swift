ae7edd0f23965c11c3599cc2d0027616372dceb9
Basheer <basheer.iapps@gmail.com>
2020-06-10 17:58:05 +0530

    Update AppDelegate.swift
    Removed FB ID

--- FBSDKSample/AppDelegate.swift
+++ FBSDKSample/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        AppEvents.loggingOverrideAppID = "fb308421999614547"
+        AppEvents.loggingOverrideAppID = "fb"
         Settings.isAutoLogAppEventsEnabled = true
         AppEvents.activateApp()
         return true
